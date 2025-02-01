d2994e4b74a0840e20d8e44ef36ad5edd68cc150
Kevin Bardou <kbardou86@gmail.com>
2025-02-01 14:59:26 +0100

    base vue

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-/** @type {import('vite').UserConfig} */
-export default {
-}
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), vue()],
+});
